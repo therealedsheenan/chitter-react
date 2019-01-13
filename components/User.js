9fb5ad9e2759224d07157e7c83dcc1337180f478
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2019-01-13 15:55:50 +0900

    add basic semantic ui setup

---
+++ components/User.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        This is the user component
+      </div>
+    );
+  }
+}
+
+export default User;
--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,9 @@
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css'
+import LoginForm from '../components/Login';
 
-export default () => <div>Hello, Chitter!</div>
+export default () => (
+  <div>
+    <LoginForm />
+  </div>
+);
